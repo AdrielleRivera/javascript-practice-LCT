0edc5fe2477df30adad5087568f2a5bbf34eb597
Adrielle Rivera <mr.adriellerivera@gmail.com>
2025-02-23 09:06:05 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
+const alertButton = document.getElementById('alertButton');
+
 // Display an alert when the alert button is clicked
 alertButton.addEventListener('click', function () {
-    alert('Hello! This is an alert message.');
+    alert('Hello! My name is Michael, how are you today?.');
 });
-c
